dd6fa29d993819bd9e43c575bc721a398ccf1efd
Ali Yalama <aliyalama@googlemail.com>
2024-07-09 23:59:24 +0200

    pages now generated properly

--- src/_data/notations.js
+++ src/_data/notations.js
@@ -2,13 +2,24 @@ const fs = require('fs');
 const path = require('path');
 
 module.exports = function() {
-  const notationsDir = __dirname + '/notations';
-  const notationFiles = fs.readdirSync(notationsDir);
+  const notationsDir = path.join(__dirname, 'notations');
+  console.log('Notations directory:', notationsDir);
   
-  return notationFiles.reduce((notations, file) => {
-    const content = fs.readFileSync(path.join(notationsDir, file), 'utf8');
-    const notation = JSON.parse(content);
-    notations[path.parse(file).name] = notation;
+  const notationFiles = fs.readdirSync(notationsDir);
+  console.log('Notation files found:', notationFiles);
+  
+  const result = notationFiles.reduce((notations, file) => {
+    if (file.endsWith('.json')) {
+      const filePath = path.join(notationsDir, file);
+      console.log('Processing file:', filePath);
+      const content = fs.readFileSync(filePath, 'utf8');
+      const notation = JSON.parse(content);
+      notations[path.parse(file).name] = notation;
+      console.log('Added notation:', path.parse(file).name);
+    }
     return notations;
   }, {});
+
+  console.log('Final notations object:', result);
+  return result;
 };
