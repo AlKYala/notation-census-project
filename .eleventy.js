3330c52c2e6eabc6968d82a58cbac8b84f3becf8
Ali Yalama <aliyalama@googlemail.com>
2024-07-10 20:58:20 +0200

    Tagging System - tags-list and tag support

--- .eleventy.js
+++ .eleventy.js
@@ -1,5 +1,37 @@
 module.exports = function(eleventyConfig) {
+  // Add support for JSON files
   eleventyConfig.addDataExtension("json", contents => JSON.parse(contents));
+
+  // Create a collection of all tags
+  eleventyConfig.addCollection("tagList", function(collectionApi) {
+    let tagSet = new Set();
+    const notations = collectionApi.items[0].data.notations;
+    for (let key in notations) {
+      const notation = notations[key];
+      if (notation.tags) {
+        notation.tags.forEach(tag => tagSet.add(tag));
+      }
+    }
+    return [...tagSet];
+  });
+
+  // Create a collection for each tag
+  eleventyConfig.addCollection("taggedNotations", function(collectionApi) {
+    const notations = collectionApi.items[0].data.notations;
+    let taggedNotations = {};
+    for (let key in notations) {
+      const notation = notations[key];
+      if (notation.tags) {
+        notation.tags.forEach(tag => {
+          if (!taggedNotations[tag]) {
+            taggedNotations[tag] = [];
+          }
+          taggedNotations[tag].push(notation);
+        });
+      }
+    }
+    return taggedNotations;
+  });
 
   return {
     dir: {
